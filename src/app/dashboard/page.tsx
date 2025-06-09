f198054de4a57f2eaf9819227cdc161b24ed04f9
Cezar D Silva <cezardsilva@gmail.com>
2025-06-09 18:33:42 -0300

    Adicionado signout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,17 @@
-
+import { headers } from "next/headers"
+import { auth } from "@/lib/auth"
 import { ButtonSignOut } from "./_components/button-signout";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
+
+  const session = await auth.api.getSession({
+    headers: await headers() // you need to pass the headers object.
+  })
+
+  if (!session) {
+    redirect("/")
+  }
 
   return (
     <div className="container mx-auto min-h-screen flex items-center justify-center flex-col">
